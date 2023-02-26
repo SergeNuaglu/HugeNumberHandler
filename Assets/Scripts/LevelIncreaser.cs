3d219febd54ca42442b1c576e1e6a892332520ee
Sergey Gvozdev <serge.nuaglu@gmail.com>
2023-02-26 14:34:35 +0200

    Created LevelIncreaser

---
+++ Assets/Scripts/LevelIncreaser.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LevelIncreaser : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _price;
+    [SerializeField] private TMP_Text _level;
+
+    private int _levelNumber = 1;
+    private int _currentRank = 0;
+    private int _denominator = 1000;
+    private float _nextLevelCost;
+    private float _minLevelCost = 0.5f;
+    private float _baseDegree = 2.6f;
+    private readonly string[] _ranks = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+                                        "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+                                        "u", "v", "w", "x","y","z" };
+
+    private void Start()
+    {
+        _price.text = (_minLevelCost * _denominator).ToString();
+        _level.text = _levelNumber.ToString();
+    }
+
+    public void Increase()
+    {
+        int minRankIndex = 0;
+
+        if (_currentRank <= minRankIndex)
+            IncreasePriceByPowerFunction();
+        else
+            IncreasePriceByPercent();
+    }
+
+    private void IncreasePriceByPowerFunction()
+    {
+        _nextLevelCost = _minLevelCost * Mathf.Pow(++_levelNumber, 2.6f + 0.013f * _levelNumber);
+        _nextLevelCost = (float)Math.Round(_nextLevelCost, 1);
+        Render();
+    }
+
+    public void IncreasePriceByPercent()
+    {
+        float rankСoefficient = 0.015f * _currentRank;
+        float stepPercent = 0.23f - rankСoefficient;
+       _nextLevelCost += _nextLevelCost * stepPercent;
+        _levelNumber++;
+        Render();
+    }
+
+    private void Render()
+    {
+        if (_nextLevelCost >= _denominator)
+        {
+            _currentRank++;
+            _nextLevelCost /= _denominator;
+
+            if (_currentRank >= _ranks.Length - 1)
+                _currentRank = _ranks.Length - 1;
+        }
+        
+        _nextLevelCost = (float)Math.Round(_nextLevelCost, 1);
+        _price.text = _nextLevelCost.ToString() + _ranks[_currentRank];
+        _level.text = _levelNumber.ToString();
+    }
+}
--- Assets/Scripts/Numbers.cs
+++ Assets/Scripts/Numbers.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class Numbers : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private string _currentAmount = "0";
+    private List<int> _sumOfBoth = new List<int>();
+    private readonly string[] _numberRanks = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", };
+
+    public void AddNumbers(string firstNumber, string secondNumber)
     {
-        
+        int carryOver = 0;
+        int rankSignsCount = 3;
+        int signsCounter;
+        int minLength = firstNumber.Length < secondNumber.Length ? firstNumber.Length : secondNumber.Length;
+        int maxLength = firstNumber.Length > secondNumber.Length ? firstNumber.Length : secondNumber.Length;
+
+        int[] digitsOfFirstNumber = ParseStringToSingleDigits(maxLength, firstNumber);
+        int[] digitsOfSecondNumber = ParseStringToSingleDigits(maxLength, secondNumber);
+        _sumOfBoth.Clear();
+
+        for (int i = 0; i < maxLength; i++)
+        {
+            int sum = (digitsOfFirstNumber[i] + digitsOfSecondNumber[i] + carryOver) % 10;
+            _sumOfBoth.Add(sum);
+            carryOver = digitsOfFirstNumber[i] + digitsOfSecondNumber[i] + carryOver >= 10 ? 1 : 0;
+
+            if(carryOver > 0 && i == maxLength - 1)
+                _sumOfBoth.Add(carryOver);
+        }
+
+        for (int i = _sumOfBoth.Count - 1; i >= 0; i--)
+        {
+            _currentAmount += _sumOfBoth[i];
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private int[] ParseStringToSingleDigits(int length, string number)
     {
-        
+        int[] digits = new int[length];
+        char point = '.';
+        char[] numberChars = number.ToCharArray();
+
+        for (int i = 0; i < number.Length; i++)
+        {
+            foreach (var sign in number)
+            {
+                foreach (var rank in _numberRanks)
+                {
+                    
+
+                }
+            }
+        }
+        for (int i = 0; i < numberChars.Length; i++)
+        {
+            string digit = numberChars[numberChars.Length - 1 - i].ToString();
+
+            if (int.TryParse(digit, out int result))
+                digits[i] = result;
+        }
+
+        return digits;
     }
 }
